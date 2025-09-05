c0f2b9fbeb8472add190365c06d0275a74263c13
Juan <juancifuentes4523@gmail.com>
2025-09-05 14:20:28 -0500

    Primer commit: subiendo las pruebas de la actividad

---
+++ prueba-brendi.py
@@ -0,0 +1,33 @@
+nombre = "brendi"
+edad= 15
+equipo= "barcelona"
+
+print(   )
+
+#print(nombre)
+#print(edad)
+#print(equipo)
+
+#print(   )
+
+#print(nombre, edad, equipo)
+#print(nombre + str(edad) + equipo)
+#print(f"hola {nombre} tengo {edad} y mi equipo favorito es {equipo}")
+
+
+numero_1=6.34
+numero_2=18.68
+
+
+print(numero_1 + numero_2)
+print(numero_1 - numero_2)
+print(numero_1 * numero_2)
+print(numero_1 / numero_2)
+print(numero_1 // numero_2)
+print(numero_1 ** numero_2)
+print(numero_1 % numero_2)
+
+print(   )
+
+suma=(numero_1 + numero_2)#esta funcion es para sumar
+print(suma)
---
+++ prueba-clase.py
@@ -0,0 +1,20 @@
+nombre = "Juan Pablo Cifuentes"
+edad = "17"
+equipo = "no me gusta el futbol"
+
+
+
+
+#f1
+print("1")
+print(nombre+ str(edad)+equipo)
+
+#f2 
+
+print("2")
+print(nombre, edad, equipo)
+
+# f3 mas usada
+
+print("3")
+print(f"hola {nombre}, tengo {edad} y {equipo}")
